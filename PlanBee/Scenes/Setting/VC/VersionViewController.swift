86b39c74bfc1de81ac686e11b77cae82f5ef9ba3
HAN JIWOOK <jiwook.han.dev@gmail.com>
2023-09-25 04:14:02 +0900

    [RENAME] : ✏️ 폴더 정리 및 변경

---
+++ PlanBee/Scenes/Setting/VC/VersionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  VersionViewController.swift
+//  PlanBee
+//
+//  Copyright (c) 2023 z-wook. All right reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class VersionViewController: UIViewController {
+    
+    private let versionView = VersionView()
+    
+    override func loadView() {
+        super.loadView()
+        
+        view = versionView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+struct VersionVCPreView: PreviewProvider {
+    static var previews: some View {
+        let versionVC = VersionViewController()
+        UINavigationController(rootViewController: versionVC)
+            .toPreview().edgesIgnoringSafeArea(.all)
+    }
+}
