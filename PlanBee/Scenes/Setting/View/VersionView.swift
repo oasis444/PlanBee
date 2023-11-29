e08504789519290c32eec93bfe0101d7a2b5cca9
jiwook_han <jiwook.han.dev@gmail.com>
2023-11-29 16:13:06 +0900

    [DESIGN] : 🎨 버전 정보 UI 변경

--- PlanBee/Scenes/Setting/View/VersionView.swift
+++ PlanBee/Scenes/Setting/View/VersionView.swift
@@ -9,10 +9,16 @@ import SnapKit
 import UIKit
 
 final class VersionView: UIView {
+    private lazy var appIconImageView: UIImageView = {
+        let view = UIImageView(image: UIImage(named: "PlanBee"))
+        return view
+    }()
+    
     private lazy var versionLabel: UILabel = {
+        let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "v1.0.0"
         let label = LabelFactory.makeLabel(
-            text: Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0.0",
-            font: ThemeFont.bold(size: 20))
+            text: "v" + version,
+            font: ThemeFont.bold(size: 22))
         label.numberOfLines = 1
         return label
     }()
@@ -30,10 +36,19 @@ final class VersionView: UIView {
 
 private extension VersionView {
     func setLayout() {
-        self.addSubview(versionLabel)
+        [appIconImageView, versionLabel].forEach {
+            self.addSubview($0)
+        }
+        
+        appIconImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview().offset(-50)
+            $0.centerX.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(50)
+            $0.height.equalTo(appIconImageView.snp.width)
+        }
         
         versionLabel.snp.makeConstraints {
-            $0.center.equalToSuperview()
+            $0.top.equalTo(appIconImageView.snp.bottom).offset(20)
             $0.leading.trailing.equalToSuperview().inset(50)
         }
     }
